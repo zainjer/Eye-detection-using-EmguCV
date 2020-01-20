3c3c8fe7ce5674cf2ae9a7fe0f3e7d114c4a8a50
zainjer <arainareeb@gmail.com>
2020-01-20 22:40:02 +0500

    working condition

--- emgu/Program.cs
+++ emgu/Program.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using Emgu;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
 
 namespace emgu
 {
@@ -12,41 +15,68 @@ namespace emgu
     {
         static void Main(string[] args)
         {
-                        
-//**************************************************************************************
-            VideoCapture webCamFeed = new VideoCapture(0);
-
+            
+            //Edit this line to change curr
+            int camIndex=1;
+            CascadeClassifier faceCascade = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+            CascadeClassifier eyeCascade = new CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml");      
+            VideoCapture webCamFeed = new VideoCapture(camIndex);
             if (!webCamFeed.IsOpened)
             {
                 Console.WriteLine("Camera not detected [Jaani camera to lagao koi]");
             }
-
             Mat frame = new Mat();
             while (true)
             {
-               webCamFeed.Read(frame);
-               CvInvoke.Imshow("Webcam", frame);
+                webCamFeed.Read(frame);
+                detectEyes(frame, faceCascade, eyeCascade);
+                CvInvoke.Imshow("Webcam", frame);
                 if (CvInvoke.WaitKey(30) >= 0)
                     break;
-            }                                                 
-        }
-
-        VideoCapture GetCamFeed(int CamIndex)
-        {
-            VideoCapture webCamFeed = new VideoCapture(0);
-
-            if (!webCamFeed.IsOpened)
-            {
-                Console.WriteLine("Camera not detected [Jaani camera to lagao koi]");
             }
+        }
+        private static void detectEyes(Mat frame, CascadeClassifier faceCascade, CascadeClassifier eyeCascade)
+        {
+            Mat grayscaleVersion = new Mat();
+            CvInvoke.CvtColor(frame, grayscaleVersion, ColorConversion.Bgr2Gray); //Dekh jaan ye method convert maarde image ko Grayscale me
+            CvInvoke.EqualizeHist(grayscaleVersion, grayscaleVersion); //ye image ki contrast bharaey ga
+            Rectangle[] faces = faceCascade.DetectMultiScale(grayscaleVersion, 2.1, 0);
 
-            return webCamFeed;
+            if (faces.Length == 0) return;
+
+            CvInvoke.Rectangle(frame, faces[0], new MCvScalar(0, 0, 255), 2);            
         }
 
-        Mat SetCamfeedToMat(VideoCapture vc)
+        //public void RunWebCam()
+        //{
+        //    VideoCapture webCamFeed = new VideoCapture(0);
+
+        //    if (!webCamFeed.IsOpened)
+        //    {
+        //        Console.WriteLine("Camera not detected [Jaani camera to lagao koi]");
+        //    }
+
+        //    Mat frame = new Mat();
+        //    while (true)
+        //    {
+        //        webCamFeed.Read(frame);
+        //        CvInvoke.Imshow("Webcam", frame);
+        //        drawFaceRectangle(frame, faceCascade, eyeCascade);
+
+        //        if (CvInvoke.WaitKey(30) >= 0)
+        //            break;
+        //    }
+        //}
+
+        public void drawFaceRectangle(Mat frame,CascadeClassifier faceCascade,CascadeClassifier eyeCascade)
         {
-            Mat frame = new Mat();
-            vc.Read(frame);
+            Mat grayscaleVersion = new Mat();
+            CvInvoke.CvtColor(frame, grayscaleVersion, ColorConversion.Bgr2Gray); //Dekh jaan ye method convert maarde image ko Grayscale me
+            CvInvoke.EqualizeHist(grayscaleVersion, grayscaleVersion);
+            //Rectangle
+
         }
     }
+
+    
 }
