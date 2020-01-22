962b343950cedf1c261e5632b27f33fe21eee10b
zainjer <arainareeb@gmail.com>
2020-01-22 23:28:04 +0500

    With eye detection but unoptimized

--- emgu/Program.cs
+++ emgu/Program.cs
@@ -37,14 +37,36 @@ namespace emgu
         }
         private static void detectEyes(Mat frame, CascadeClassifier faceCascade, CascadeClassifier eyeCascade)
         {
-            Mat grayscaleVersion = new Mat();
-            CvInvoke.CvtColor(frame, grayscaleVersion, ColorConversion.Bgr2Gray); //Dekh jaan ye method convert maarde image ko Grayscale me
-            CvInvoke.EqualizeHist(grayscaleVersion, grayscaleVersion); //ye image ki contrast bharaey ga
-            Rectangle[] faces = faceCascade.DetectMultiScale(grayscaleVersion, 2.1, 0);
+            Mat grayscaleVersion = new Mat();  //cv::Mat grayscale;
 
-            if (faces.Length == 0) return;
+            CvInvoke.CvtColor(frame, grayscaleVersion, ColorConversion.Bgr2Gray); //cv::cvtColor(frame, grayscale, CV_BGR2GRAY);   //Dekh jaan ye method convert maarde image ko Grayscale me
 
-            CvInvoke.Rectangle(frame, faces[0], new MCvScalar(0, 0, 255), 2);            
+            CvInvoke.EqualizeHist(grayscaleVersion, grayscaleVersion); //cv::equalizeHist(grayscale, grayscale);  //ye image ki contrast bharaey ga
+
+            Rectangle[] faces; //  std::vector<cv::Rect> faces;
+
+            faces = faceCascade.DetectMultiScale(grayscaleVersion, 1.2, 0); //faceCascade.detectMultiScale(grayscale, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(150, 150));           
+
+            if (faces.Length == 0) return; //if (faces.size() == 0) return;  // none face was detected
+
+            
+
+            Mat face = new Mat(frame,faces[0]); //  cv::Mat face = frame(faces[0]); // crop the face
+
+            Rectangle[] eyes; // std::vector<cv::Rect> eyes;
+
+            eyes = eyeCascade.DetectMultiScale(face, 1.2, 0); //eyeCascade.detectMultiScale(face, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(150, 150));
+
+            CvInvoke.Rectangle(frame, faces[0], new MCvScalar(0, 0, 255), 1); //drawing a face on Frame
+
+            if (eyes.Length ==0) return;
+
+            foreach (Rectangle eye in eyes)
+            {
+                CvInvoke.Rectangle(frame, eye, new MCvScalar(255, 255, 0), 1);
+            }
+
+
         }
 
         //public void RunWebCam()
@@ -68,14 +90,6 @@ namespace emgu
         //    }
         //}
 
-        public void drawFaceRectangle(Mat frame,CascadeClassifier faceCascade,CascadeClassifier eyeCascade)
-        {
-            Mat grayscaleVersion = new Mat();
-            CvInvoke.CvtColor(frame, grayscaleVersion, ColorConversion.Bgr2Gray); //Dekh jaan ye method convert maarde image ko Grayscale me
-            CvInvoke.EqualizeHist(grayscaleVersion, grayscaleVersion);
-            //Rectangle
-
-        }
     }
 
     
